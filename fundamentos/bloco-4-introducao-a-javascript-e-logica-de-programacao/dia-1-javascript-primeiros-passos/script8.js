6160428f01d21826b763634ab83879fa064f34f5
douglas oliveira <douglasgama0@gmail.com>
2021-07-28 20:48:15 -0300

    finalizaçao do script8

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script8.js
@@ -0,0 +1,13 @@
+let a = 3
+let b = 7
+let c = 5
+
+if (a % 2 == 0) {
+  console.log('O número é Par')
+} else if (b % 2 == 0) {
+  console.log('O número é Par')
+} else if (c % 2 == 0) {
+  console.log('O número é Par')
+} else {
+  console.log('Nenhum dos números são Pares')
+}
