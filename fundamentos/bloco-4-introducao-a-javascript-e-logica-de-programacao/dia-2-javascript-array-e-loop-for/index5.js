7e4b335b7bfa4d11872244ce6799094135568f43
douglas oliveira <douglasgama0@gmail.com>
2021-07-29 19:21:13 -0300

    finalizaçao parte 5 do exercicios ()

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index5.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index5.js
@@ -1,8 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
-let somaNumbers = 0
-let mediaNumbers = 0
+let maiorNumero = numbers[0]
 
 for (let index = 0; index < numbers.length; index++) {
-  somaNumbers += numbers[index]
-  mediaNumbers = somaNumbers / numbers.length
+  if (numbers[index] > maiorNumero) {
+    maiorNumero = numbers[index]
+  }
 }
+console.log(maiorNumero)
