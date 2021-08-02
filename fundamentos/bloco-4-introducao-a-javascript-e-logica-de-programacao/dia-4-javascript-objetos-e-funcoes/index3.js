ba64141489e9167f14f989f0d17127c0e6d15ea4
douglas oliveira <douglasgama0@gmail.com>
2021-08-02 18:57:34 -0300

    Finalizaçao da 5 parte do exercicio parte I

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/index3.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/index3.js
@@ -1,14 +1,22 @@
+let text = ' '
 let info = {
-  personagem: 'Margarida',
+  nome: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  recorrente: 'Sim'
+  recorrente: 'Sim',
+  personagem2: {
+    nome: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+  }
 }
 
-for (let key in info) {
-  console.log(key)
-}
-console.log(' ')
-for (let key in info) {
-  console.log(info[key])
+console.log(info.nome + ' e ' + info.personagem2.nome)
+console.log(info.origem + ' e ' + info.personagem2.origem)
+console.log(info.nota + ' e ' + info.personagem2.nota)
+if (info.recorrente == info.personagem2.recorrente) {
+  console.log('Ambos recorrentes')
+} else {
+  console.log('batata')
 }
