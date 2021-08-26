191868ecaeb1bcdab84a625553147ad119e8bd08
douglas oliveira <douglasgama0@gmail.com>
2021-08-26 12:42:18 -0300

    Ajuste da Arrow Function e template literals

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,21 +1,25 @@
-function testingScope(escopo) {
-  if (escopo === true) {
-    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)'
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !'
-    console.log(ifScope)
-  } else {
-    var elseScope = 'Não devo ser utilizada fora meu escopo (else)'
-    console.log(elseScope)
-  }
-  console.log(ifScope + ' o que estou fazendo aqui ? :O') // Se necessário esta linha pode ser removida.
-}
+// function testingScope(escopo) {
+//   if (escopo === true) {
+//     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)'
+//     ifScope = ifScope + ' ótimo, fui utilizada no escopo !'
+//     console.log(ifScope)
+//   } else {
+//     var elseScope = 'Não devo ser utilizada fora meu escopo (else)'
+//     console.log(elseScope)
+//   }
+//   console.log(ifScope + ' o que estou fazendo aqui ? :O') // Se necessário esta linha pode ser removida.
+// }
 
-testingScope(true)
+// testingScope(true)
 
 //Resolução Parte I do exercicio, dia 7.1
 
+const ifScope = 'Não devo ser utilizada fora do meu escopo (if)'
+const elseScope = 'Não devo ser utilizada fora meu escopo (else)'
 const testingScope = escopo => {
   escopo
-    ? console.log('Não devo ser utilizada fora do meu escopo (if)')
-    : console.log('Não devo ser utilizada fora meu escopo (else)')
+    ? console.log(`${ifScope} ótimo, fui utilizada no escopo !`)
+    : console.log(elseScope)
 }
+
+testingScope(true)
