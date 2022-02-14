1b4cb0ccaccfb4fdc64b36791a814c7bd9c1287a
douglas oliveira <douglasgama0@gmail.com>
2022-02-14 16:02:29 -0300

    feat: req03

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/script1.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/script1.js
@@ -13,12 +13,15 @@ function randomNumber() {
   return Math.floor(Math.random() * 100 + 1);
 }
 
-function createNumber() {
+async function createNumber() {
   const arrayNumb = ['num1', 'num2', 'num3'];
   const radomicNumbers = arrayNumb.map(randomNumber);
-  myfunc(...radomicNumbers)
-    .then(result => console.log(result))
-    .catch(err => console.error(err.message))
+  try {
+    const response = await myfunc(...radomicNumbers);
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 createNumber();
