8c75e8ccc41d374717b7d92d6917759746c678f8
douglas oliveira <douglasgama0@gmail.com>
2022-02-14 15:28:47 -0300

    finalizando exercicio 1

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/script1/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/script1/index.js
@@ -1,3 +1,10 @@
 function myfunc(num1, num2, num3) {
-  return new Promise((resolve, reject) => {});
+  return new Promise((resolve, reject) => {
+    if (typeof num1 !== Number, typeof num2 !== Number, typeof num3 !== Number) {
+      reject('Informe apenas números');
+    }
+    const result = (num1 + num2) * num3;
+    result < 50 && reject('Valor muito baixo');
+    resolve(result);
+  });
 }
