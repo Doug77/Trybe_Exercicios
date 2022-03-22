ed1f712153624781346af1a332d9639c67a81304
douglas oliveira <douglasgama0@gmail.com>
2022-03-22 16:40:32 -0300

    exercicio 02

--- back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/length.ts
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/length.ts
@@ -1,12 +1,12 @@
 const units = ["km", "hm", "dam", "m", "dm", "cm", "mm"];
 
-function makeError(unity: string) {
-    throw new Error(`A unidade ${unity} não é uma unidade válida.`)
+function makeErro(unity: string) {
+  throw new Error(`A unidade ${unity} não é uma unidade válida.`)
 }
 
-function convert(value: number, baseUnity: string, convertedUnity: string): number {
-  if (!units.includes(baseUnity)) makeError(baseUnity);
-  if (!units.includes(convertedUnity)) makeError(convertedUnity);
+function convertUnity(value: number, baseUnity: string, convertedUnity: string): number {
+  if (!units.includes(baseUnity)) makeErro(baseUnity);
+  if (!units.includes(convertedUnity)) makeErro(convertedUnity);
 
   const forIndex = units.indexOf(baseUnity);
   const toIndex = units.indexOf(convertedUnity);
@@ -16,4 +16,4 @@ function convert(value: number, baseUnity: string, convertedUnity: string): numb
   console.log('fim');
 }
 
-convert(300, 'km', 'dm');
+convertUnity(300, 'km', 'dm');
